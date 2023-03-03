65d8e06594d1560c471c3c246551c146aea9ff35
Piet Maier <pietmaier.github@protonmail.com>
2023-03-03 13:20:34 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Reproduction for [Vercel Discussion #1661](https://github.com/orgs/vercel/discussions/1661)
+
+Locally / Anywhere Except Vercel Deployments:
+
+1. Run `npm run generate` & `npm run preview`
+2. Success
+
+Vercel Deployments:
+
+1. Deploy
+2. Images Throw 404 Error
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <nuxt-img src="/vercel-icon-light.png" width="40" />
+</template>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  height: 100%;
+  background-color: black;
+}
+</style>
